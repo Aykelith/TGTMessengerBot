8a7590b08a4e1d3dfa0ba4408471698313406ee6
Prisacariu Alexandru <zalexxanderx@gmail.com>
2017-08-01 16:27:36 +0300

    ...

--- src/app.js
+++ src/app.js
@@ -10,100 +10,115 @@
 /* jshint node: true, devel: true */
 'use strict';
 
-import MessengerAPI from 'MessengerAPI';
-import Database from 'Database';
-import ComingOutApp from 'ComingOutApp';
-import GamesApp from 'GamesApp';
-import NicoApp from 'NicoApp';
-
-import express from 'express';
-import https from 'https';
-import fs from 'fs';
-
-var request = require('request');
-
-import path from 'path';
-
+const cluster = require('cluster');
 const numCPUs = require('os').cpus().length;
 console.log(`Number of cores:${numCPUs}`);
 
-var app = express();
-app.set('port', process.env.PORT || 3000);
-app.set('view engine', 'ejs');
-app.set('views', path.join(__dirname, 'views'));
-app.use(express.static(path.join(__dirname, 'public')));
+if (cluster.isMaster) {
+    console.log(`Master ${process.pid} is running`);
 
-const ADMIN_ID = '1256338984475739';
+    // Fork workers.
+    for (let i = 0; i < numCPUs; i++) {
+        cluster.fork();
+    }
 
-var messenger    = new MessengerAPI(app);
-var db           = new Database();
-var comingOutApp = new ComingOutApp(db, messenger);
-var gamesApp     = new GamesApp(db, messenger);
-var nicoApp      = new NicoApp(db, messenger);
+    cluster.on('exit', (worker, code, signal) => {
+        console.log(`worker ${worker.process.pid} died`);
+        cluster.fork();
+    });
+} else {
+    import MessengerAPI from 'MessengerAPI';
+    import Database from 'Database';
+    import ComingOutApp from 'ComingOutApp';
+    import GamesApp from 'GamesApp';
+    import NicoApp from 'NicoApp';
 
-messenger.setReceivedMessageHandler((event) => {
-    var senderID = event.sender.id;
+    import express from 'express';
+    import https from 'https';
+    import fs from 'fs';
 
-    var message = event.message;
-    var messageText = message.text;
+    var request = require('request');
 
-    if (messageText) {
-        if (!db.userExists(senderID)) {
-            if (!db.users[senderID]) {
-                messenger.sendTextMessage(senderID, "Bine ai venit!\nCum te cheama(prenumele)?");
-                db.insertTemporaryUser(senderID);
-                return;
-            } else {
-                db.insertUser(senderID, messageText);
-                messenger.sendTextMessage(senderID, `Bine ai venit, ${messageText}!`);
-                return;
-            }
-        }
+    import path from 'path';
 
-        if (comingOutApp.receivedMessage(senderID, messageText)) return;
-        if (gamesApp.receivedMessage(senderID, messageText)) return;
-		if (nicoApp.receivedMessage(senderID, messageText)) return;
+    var app = express();
+    app.set('port', process.env.PORT || 3000);
+    app.set('view engine', 'ejs');
+    app.set('views', path.join(__dirname, 'views'));
+    app.use(express.static(path.join(__dirname, 'public')));
 
-        var msgLowerCase = messageText.toLowerCase();
+    const ADMIN_ID = '1256338984475739';
 
-        if (msgLowerCase.indexOf('nume:') === 0) {
-            var name = messageText.substring(5);
-            db.changeUserName(senderID, name);
-            messenger.sendTextMessage(senderID, `Noul tau nume este \'${name}\'`);
-            return;
-        } else if (msgLowerCase.indexOf('caine') !== -1) {
-            request('http://thedogapi.co.uk/api/v1/dog', function (error, response, body) {
-                if (response.statusCode != 200) {
-                    messenger.sendTextMessage(senderID, "Ma simt cam prost momentan...");
+    var messenger    = new MessengerAPI(app);
+    var db           = new Database();
+    var comingOutApp = new ComingOutApp(db, messenger);
+    var gamesApp     = new GamesApp(db, messenger);
+    var nicoApp      = new NicoApp(db, messenger);
+
+    messenger.setReceivedMessageHandler((event) => {
+        var senderID = event.sender.id;
+
+        var message = event.message;
+        var messageText = message.text;
+
+        if (messageText) {
+            if (!db.userExists(senderID)) {
+                if (!db.users[senderID]) {
+                    messenger.sendTextMessage(senderID, "Bine ai venit!\nCum te cheama(prenumele)?");
+                    db.insertTemporaryUser(senderID);
+                    return;
+                } else {
+                    db.insertUser(senderID, messageText);
+                    messenger.sendTextMessage(senderID, `Bine ai venit, ${messageText}!`);
                     return;
                 }
+            }
 
-                var url = JSON.parse(body).data[0].url;
-                messenger.sendImageMessage(senderID, url);
-            });
-        } else if (msgLowerCase.indexOf('help') === 0) {
-            messenger.sendTextMessage(senderID, `Lista de cuvinte:\n
+            if (comingOutApp.receivedMessage(senderID, messageText)) return;
+            if (gamesApp.receivedMessage(senderID, messageText)) return;
+    		if (nicoApp.receivedMessage(senderID, messageText)) return;
+
+            var msgLowerCase = messageText.toLowerCase();
+
+            if (msgLowerCase.indexOf('nume:') === 0) {
+                var name = messageText.substring(5);
+                db.changeUserName(senderID, name);
+                messenger.sendTextMessage(senderID, `Noul tau nume este \'${name}\'`);
+                return;
+            } else if (msgLowerCase.indexOf('caine') !== -1) {
+                request('http://thedogapi.co.uk/api/v1/dog', function (error, response, body) {
+                    if (response.statusCode != 200) {
+                        messenger.sendTextMessage(senderID, "Ma simt cam prost momentan...");
+                        return;
+                    }
+
+                    var url = JSON.parse(body).data[0].url;
+                    messenger.sendImageMessage(senderID, url);
+                });
+            } else if (msgLowerCase.indexOf('help') === 0) {
+                messenger.sendTextMessage(senderID, `Lista de cuvinte:\n
 nume:[NUME NOU] - iti schimbi numele\n
 iesire:[]...`);
-        } else {
-            messenger.sendTextMessage(senderID, 'Nu inteleg ce vrei sa spui...');
+            } else {
+                messenger.sendTextMessage(senderID, 'Nu inteleg ce vrei sa spui...');
+            }
         }
-    }
-});
+    });
 
-messenger.setReceivedPostbackHandler((event) => {
-    var senderID = event.sender.id;
-    var payload = event.postback.payload;
+    messenger.setReceivedPostbackHandler((event) => {
+        var senderID = event.sender.id;
+        var payload = event.postback.payload;
 
-    if (comingOutApp.receivedPostback(senderID, payload)) return;
-    if (gamesApp.receivedPostback(senderID, payload)) return;
-});
+        if (comingOutApp.receivedPostback(senderID, payload)) return;
+        if (gamesApp.receivedPostback(senderID, payload)) return;
+    });
 
-// Start server
-// Webhooks must be available via SSL with a certificate signed by a valid
-// certificate authority.
-app.listen(app.get('port'), function() {
-    console.log('Node app is running on port', app.get('port'));
-});
+    // Start server
+    // Webhooks must be available via SSL with a certificate signed by a valid
+    // certificate authority.
+    app.listen(app.get('port'), function() {
+        console.log('Node app is running on port', app.get('port'));
+    });
 
-module.exports = app;
+    console.log(`Worker ${process.pid} started`);
+}
