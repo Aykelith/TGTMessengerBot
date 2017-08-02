059a56b169891de6ce585c78813274d24aff6375
Prisacariu Alexandru <zalexxanderx@gmail.com>
2017-08-02 14:44:22 +0300

    ...

--- src/app.js
+++ src/app.js
@@ -93,6 +93,8 @@ messenger.setReceivedMessageHandler((event) => {
             messenger.sendTextMessage(senderID, EDI_QUOTES[randomIntFromInterval(0, EDI_QUOTES.length-1)]);
         } else if (msgLowerCase.indexOf('robu') === 0) {
             messenger.sendTextMessage(senderID, 'Numele aleatoriu selectat este: ' + db.users[db.users_ids[randomIntFromInterval(0, db.users_ids.length-1)]].name);
+        } else if (msgLowerCase.indexOf('alex') === 0) {
+            messenger.sendTextMessage(senderID, randomIntFromInterval(0, 1) ? 'TITIREZ!' : 'Tiramisu');
         } else if (msgLowerCase.indexOf('help') === 0) {
             messenger.sendTextMessage(senderID, `Lista de cuvinte:\n
 nume:[NUME NOU] - iti schimbi numele\n
